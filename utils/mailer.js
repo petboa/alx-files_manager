d25e4ecefbf5ae48590f7b3ad3d55c491be284eb
petboa <petboa123@gmail.com>
2023-12-10 10:04:29 -0800

    file manager

---
+++ utils/mailer.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import fs from 'fs';
+import readline from 'readline';
+import { promisify } from 'util';
+import mimeMessage from 'mime-message';
+import { gmail_v1 as gmailV1, google } from 'googleapis';
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+async function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.error('Error retrieving access token', err);
+        return;
+      }
+      oAuth2Client.setCredentials(token);
+      writeFileAsync(TOKEN_PATH, JSON.stringify(token))
+        .then(() => {
+          console.log('Token stored to', TOKEN_PATH);
+          callback(oAuth2Client);
+        })
+        .catch((writeErr) => console.error(writeErr));
+    });
+  });
+}
+
+/**
